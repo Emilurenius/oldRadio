00e07c0d6cad06b1251250e3910287829127341d
Emil Christiansen <emilsen68@gmail.com>
2022-05-18 20:34:13 +0200

    first commit

---
+++ youtubePlayer.py
@@ -0,0 +1,18 @@
+import vlc, pafy, time
+
+def youtubeURL(watchID):
+    return f'http://www.youtube.com/watch?v={watchID}'
+
+def main():
+    video = pafy.new(youtubeURL('dQw4w9WgXcQ'))
+    best = video.getbest()
+    media = vlc.MediaPlayer(best.url)
+    print('Playing media')
+    media.play()
+    time.sleep(5)
+    while media.is_playing():
+        time.sleep(1)
+    
+
+if __name__ == '__main__':
+    main()

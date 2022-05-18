275327884990afbab32d37c215fb11ec8955a8e4
Emil Christiansen <emilsen68@gmail.com>
2022-05-18 23:21:20 +0200

    Ability to play more songs

--- youtubePlayer.py
+++ youtubePlayer.py
@@ -4,15 +4,15 @@ def youtubeURL(watchID):
     return f'http://www.youtube.com/watch?v={watchID}'
 
 def main():
-    video = pafy.new(youtubeURL('dQw4w9WgXcQ'))
+    video = pafy.new(input('Paste full link of video: >> '))
     best = video.getbestaudio()
     media = vlc.MediaPlayer(best.url)
     print('Playing media')
     media.play()
-    time.sleep(5)
-    while media.is_playing():
-        time.sleep(1)
+    input('Press enter to stop song')
+    media.stop()
     
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
